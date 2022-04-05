fd437d15afe72dd2729d3e43ec02c1616a1175d1
Vaish Shanmugam <aardhana@asu.edu>
2022-04-05 17:21:48 -0400

    Delete MathworksTest.java

--- MathworksTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Assert;
-import org.junit.Test;
-
-public class MathworksTest {
-	@Test
-	public void isTwinTest() {
-		Assert.assertFalse(MathworksCoding.isTwin("adada", "abacd"));
-	}
-}
